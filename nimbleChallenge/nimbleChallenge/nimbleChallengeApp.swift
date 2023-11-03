fdb9d333960c4b31aa013ac0c0139c3304f845aa
mario saldaña martinez <msalda53@gmail.com>
2023-11-03 17:38:53 -0600

    setting project

---
+++ nimbleChallenge/nimbleChallenge/nimbleChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nimbleChallengeApp.swift
+//  nimbleChallenge
+//
+//  Created by mario saldaña martinez on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct nimbleChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
